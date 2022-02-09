449da32ce14e67d591c62545c1421697c4357af4
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2022-02-09 15:01:46 +0530

    first commit

---
+++ defi-staking-app-starter/truffle-config.js
@@ -0,0 +1,32 @@
+//We bring in babel
+require('babel-register');
+require('babel-polyfill');
+
+//We are creating our export over here and then we are setting up our networks to the ganache network
+//And the idea of the network is going to be whatever the network is about 
+//We have our port set up to ganache 
+module.exports = {
+    networks:{
+        development:{ 
+            host: '127.0.0.1:7545',
+            port: '7545',
+            network_id: '*'//Connect or match to any network
+
+        },
+
+    },
+    //Contracts Directory for info to truffle
+    contracts_directory: './src/contracts/',
+    //We also want to build our directories for our JSON files
+    comtracts_build_directory: './src/truffle_abis/',
+    //solidity compiler setup
+    compilers:{
+        solc:{ 
+            version:'^0.5.0',//anything above 0.5.0
+            optimizer: { 
+                enabled: true,
+                runs: 200
+            }
+        }
+    }
+}
